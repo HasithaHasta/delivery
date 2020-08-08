1e8ce934cfa43a8da71afa68f575415211f35365
HasithaHasta <56692282+HasithaHasta@users.noreply.github.com>
2020-08-08 16:16:28 +0530

    Update urls.py

--- pizzaapp/urls.py
+++ pizzaapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import userloginview,signupuser,homepageview,adminloginview,adminhomepageview,authenticateadmin,logoutadmin,addpizza,deletepizza
+from .views import customerwelcomeview,userauthenticate,userloginview,signupuser,homepageview,adminloginview,adminhomepageview,authenticateadmin,logoutadmin,addpizza,deletepizza
 
 urlpatterns = [
    path('admin/',adminloginview,name = 'adminloginpage'),
@@ -12,5 +12,6 @@ urlpatterns = [
    path('',homepageview,name = 'homepage'),
    path('signupuser/',signupuser),
    path('loginuser/',userloginview,name = 'userloginpage'),
-   path('customerwelcomeview/',customerwelcomeview,name = 'customerpage')
+   path('customerwelcomeview/',customerwelcomeview,name = 'customerpage'),
+   path('customer/authenticate/',userauthenticate)
 ]
